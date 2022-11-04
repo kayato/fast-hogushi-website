742640143799d066d4cd80c25ff445c7202360f8
kayato <h.kato345@gmail.com>
2022-11-05 00:10:02 +0530

    Update first version

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="" />
+	<link rel="icon" type="image/png" href="fav.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="icon.png" />
+	<title>ファストほぐし</title>
+	<link rel="canonical" href="" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:wght@700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<body class="top">
+	<div class="container">
+		<header class="header">
+			<div class="header__top">
+				<h1 class="header__title">ファストほぐし</h1>
+			</div>
+			<nav class="gnav">
+				<ul class="gnav__list" role="list">
+					<li class="gnav__item"><a href="#flow">使い方の流れ</a></li>
+					<li class="gnav__item"><a href="#price">料金</a></li>
+					<li class="gnav__item"><a href="#shops">店舗一覧</a></li>
+					<li class="gnav__item"><a href="#faq">FAQ</a></li>
+					<li class="gnav__item"><a href="">メニュー確認用</a></li>
+					<li class="gnav__item"><a href="">メニュー確認用</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<div class="kv">
+				<div class="kv__container">
+					<ul class="kv__prices" role="list">
+						<li class="kv__price">10分間<br>1,100円</li>
+						<li class="kv__price">20分間<br>2,200円</li>
+					</ul>
+				</div>
+				<picture class="kv__thumbnail">
+					<source type="image/png" srcset="./img/kv-pc.jpg" media="(min-width: 600px)" width="856" height="480">
+					<source type="image/png" srcset="./img/kv.jpg" width="428" height="285">
+					<img src="./img/kv.jpg" width="428" height="285" alt="施術を受ける男性の様子" decoding="async">
+				</picture>
+			</div>
+
+			<section class="about">
+				<div class="about__container">
+					<h2 class="section__title">ファストほぐしとは？</h2>
+					<p class="about__text">
+						10分間で今すぐマッサージガンを利用した<br>
+						ほぐし施術ができる。
+					</p>
+					<p class="about__text">
+						<strong>予約不要・10分間で1,100円の格安価格</strong>
+					</p>
+					<p class="about__text">
+						なんとなく疲れている。<br>
+						整体に行きたいけれど忙しい。<br>
+						気軽に行きたいのに予約が必要。
+					</p>
+					<p class="about__text">
+						「10分間で今すぐ行ける」がファストほぐしの強みです。
+					</p>
+					<p class="about__text">
+						<em>
+							美容院の空き時間を利用したサービスなので、
+							提携している美容院で施術となります。
+						</em>
+					</p>
+				</div>
+			</section>
+
+			<section class="price" id="price">
+				<div class="price__container">
+					<h2 class="section__title">料金</h2>
+					<ul class="price__list" role="list">
+						<li class="price__item"><span>10分間</span>1,100円（税込）</li>
+						<li class="price__item"><span>20分間</span>2,200円（税込）</li>
+					</ul>
+				</div>
+			</section>
+
+			<section class="merit">
+				<div class="merit__container">
+					<h2 class="section__title" data-type="large">マッサージガン施術のメリット</h2>
+					<p class="merit__text">
+						ファストほぐしでは、短時間で大きな効果を得るためマッサージガン利用して施術をおこないます。
+					</p>
+					<ol class="merit__list" role="list">
+						<li class="merit__item">
+							<div class="merit__contents">
+								<h3 class="merit__sub-title"><span class="merit__number">メリット<span>1</span></span>短時間で効果が出る</h3>
+								<p class="merit__msg">
+									マッサージガンの利用目安は10分から20分間と言われています。<em>振動を使って短時間で一気に効果を発揮できるので、短い時間の施術に向いています。</em>
+								</p>
+								<p class="merit__msg">
+									科学的にも、<em>長いマッサージを実施する１度受けるよりも、短いマッサージを多くの回数受ける方がリラックス効果が高い</em>というのが実証されています。
+								</p>
+							</div>
+							<img src="./img/video1.gif" alt="" class="merit__thumbnail" decoding="async">
+						</li>
+						<li class="merit__item">
+							<div class="merit__contents">
+								<h3 class="merit__sub-title"><span class="merit__number">メリット<span>2</span></span>筋膜リリース効果</h3>
+								<p class="merit__msg">
+									マッサージガンの<em>先端部分の振動が筋膜の「こわばり」をほぐすことにより、身体の不調を改善・免疫や病気の予防</em>にもなります。<br>
+									筋膜リリースは、手の施術で再現することは可能ですが非常に難しいテクニックです。
+								</p>
+							</div>
+							<img src="./img/image2.jpg" alt="" class="merit__thumbnail" decoding="async">
+						</li>
+						<li class="merit__item">
+							<div class="merit__contents">
+								<h3 class="merit__sub-title"><span class="merit__number">メリット<span>3</span></span>人にマッサージガンを<br>やってもらうと効果倍増</h3>
+								<p class="merit__msg">
+									マッサージガンを自分で買ってやればいいのでは？と思うかもしれませんが、<em>自分でやるとほぐしたい場所に力が入ってしまい、ほぐし効果が軽減</em>してしまいます。
+								</p>
+								<p class="merit__msg">筋肉をリラックスした状態で、的確に筋肉をほぐすことが可能となります。</p>
+							</div>
+							<img src="./img/image3.jpg" alt="" class="merit__thumbnail" decoding="async">
+						</li>
+					</ol>
+				</div>
+			</section>
+
+			<section class="flow" id="flow">
+				<div class="flow__container">
+					<h2 class="section__title" data-type="large">使い方の流れ</h2>
+					<ol class="flow__list" role="list">
+						<li class="flow__item">
+							<div class="flow__contents">
+								<h3 class="flow__sub-title"><span class="flow__number">Step<span>1</span></span>Googleマップで<br>「ファストほぐし」で検索</h3>
+								<p class="flow__msg">お近くのファストほぐしを検索して下さい。</p>
+							</div>
+							<img src="./img/image4.jpg" alt="" class="flow__thumbnail" decoding="async">
+						</li>
+						<li class="flow__item">
+							<div class="flow__contents">
+								<h3 class="flow__sub-title"><span class="flow__number">Step<span>2</span></span>店に電話して<br>「今から行けるか」確認</h3>
+								<p class="flow__msg">お近くの店に今から行けるか確認して下さい。</p>
+							</div>
+							<img src="./img/image5.jpg" alt="" class="flow__thumbnail" decoding="async">
+						</li>
+						<li class="flow__item">
+							<div class="flow__contents">
+								<h3 class="flow__sub-title"><span class="flow__number">Step<span>3</span></span>施術</h3>
+								<p class="flow__msg">マッサージガンを利用して、首・肩・背中を中心とした施術を実施します。施術は10分か、20分間をご選択できます。</p>
+								<p class="flow__msg">※店によっては、座りながらの施術となります。</p>
+							</div>
+							<img src="./img/image6.jpg" alt="" class="flow__thumbnail" decoding="async">
+						</li>
+						<li class="flow__item">
+							<div class="flow__contents">
+								<h3 class="flow__sub-title"><span class="flow__number">Step<span>4</span></span>現地決済</h3>
+								<p class="flow__msg">現金かクレジットカードの支払いとなります。<br>
+									恐れ入りますが、その他の決済方法が可能かについては、各店舗によりますので、電話時にご確認ください。</p>
+							</div>
+							<img src="./img/image7.jpg" alt="" class="flow__thumbnail" decoding="async">
+						</li>
+					</ol>
+				</div>
+			</section>
+
+			<section class="shops" id="shops">
+				<div class="shops__container">
+					<h2 class="section__title">店舗一覧</h2>
+					<ul class="shops__list" role="list">
+						<li class="shops__item">
+							<h3 class="shops__sub-title">ファストほぐし大泉学園1号店</h3>
+							<p class="shops__address">
+								〒178-0063 東京都練馬区東大泉1丁目30−13<br>ビクトヘア内
+							</p>
+							<a href="tel:0899104790" class="shops__tel">089-910-4790</a>
+							<iframe class="shops__map"
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.025362180145!2d139.5844512156254!3d35.75018138017826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018eec7b55fac43%3A0xd434140d49837cc5!2z44CSMTc4LTAwNjMg5p2x5Lqs6YO957e06aas5Yy65p2x5aSn5rOJ77yR5LiB55uu77yT77yQ4oiS77yR77yT!5e0!3m2!1sja!2sjp!4v1667434243547!5m2!1sja!2sjp"
+								style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+						</li>
+						<li class="shops__item">
+							<h3 class="shops__sub-title">ファストほぐし大泉学園2号店</h3>
+							<p class="shops__address">
+								〒178-0063 東京都練馬区東大泉1丁目26−19<br>hair moss内
+							</p>
+							<a href="tel:08012272775" class="shops__tel">080-1227-2775</a>
+							<iframe class="shops__map"
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.0555784175385!2d139.58590941562542!3d35.74943868017847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018eec77415dfb3%3A0x6b68dae552f881ac!2z44CSMTc4LTAwNjMg5p2x5Lqs6YO957e06aas5Yy65p2x5aSn5rOJ77yR5LiB55uu77yS77yW4oiS77yR77yZ!5e0!3m2!1sja!2sjp!4v1667434344740!5m2!1sja!2sjp"
+								style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<section class="faq" id="faq">
+				<div class="faq__container">
+					<h2 class="section__title" data-type="large">よくある質問</h2>
+					<details class="faq__item">
+						<summary class="faq__q">提携店舗とは何ですか？</summary>
+						<span>
+							腰のみの施術は行っておりません。背中を長く施術することは可能です。
+						</span>
+					</details>
+					<details class="faq__item">
+						<summary class="faq__q">提携店舗とは何ですか？</summary>
+						<span>
+							腰のみの施術は行っておりません。背中を長く施術することは可能です。
+						</span>
+					</details>
+					<details class="faq__item">
+						<summary class="faq__q">提携店舗とは何ですか？</summary>
+						<span>
+							腰のみの施術は行っておりません。背中を長く施術することは可能です。
+						</span>
+					</details>
+					<details class="faq__item">
+						<summary class="faq__q">提携店舗とは何ですか？</summary>
+						<span>
+							腰のみの施術は行っておりません。背中を長く施術することは可能です。
+						</span>
+					</details>
+				</div>
+			</section>
+		</main>
+		<footer class="footer">
+			<small class="footer__copy">2022 &copy; ファストほぐし</small>
+		</footer>
+	</div>
+</body>
+
+</html>
